c520e38a61471073e0d8ca808bfd21feb6ba0873
phi1 <UUONUUORd@gmail.com>
2026-07-24 13:21:01 +0000

    fix server output to dist/index.js

--- script/build.ts
+++ script/build.ts
@@ -66,7 +66,7 @@ async function buildAll() {
     platform: "node",
     bundle: true,
     format: "cjs",
-    outfile: "dist/index.cjs",
+    outfile: "dist/index.js",
     define: {
       "process.env.NODE_ENV": '"production"',
     },
