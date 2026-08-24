193d203fe33a1a59a82adbac3c79fef7ff35496c
phi1 <UUONUUORd@gmail.com>
2026-08-24 20:01:07 +0200

    Update vite.config.ts
    Signed-off-by: phi1 <UUONUUORd@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import glsl from "vite-plugin-glsl";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     glsl(),
   ],
   resolve: {
